c4b0fe639ae4be57c0cc598089cbda167a5cbf6e
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-30 20:54:11 +0700

    Update user-script.js

--- user-script.js
+++ user-script.js
@@ -1,1 +1,13 @@
-
+// ==UserScript==
+// @name         Simple Bypass Notification
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Bypass Volcano - Work.ink CAPTCHA with a simple notification system
+// @author       Huyyhere
+// @match        https://key.volcano.wtf/*
+// @match        https://work.ink/*
+// @grant        unsafeWindow
+// @run-at       document-start
+// @downloadURL https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/refs/heads/main/work.ink-bypass.js
+// @updateURL https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/refs/heads/main/work.ink-bypass.js
+// ==/UserScript==
