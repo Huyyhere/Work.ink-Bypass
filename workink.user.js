9b853ecdac33fd48dff1b706b246c3555697cb4b
Huyyhere <bquangghuyy2011@gmail.com>
2025-10-31 12:22:55 +0700

    Update workink.user.js

--- workink.user.js
+++ workink.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Bypass Work.ink
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
-// @description  Bypass Volcano - Work.ink
+// @version      1.0.1
+// @description  Bypass Work.ink
 // @author       Huyyhere
-// @match        https://key.volcano.wtf/*
 // @match        https://work.ink/*
 // @grant        unsafeWindow
 // @run-at       document-start
 // @downloadURL  https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/refs/heads/main/work.ink-bypass.js
-// @updateURL    https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/refs/heads/main/work.ink-bypass.js
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/main/workink.user.js
 // @require      https://raw.githubusercontent.com/Huyyhere/Work.ink-Bypass/refs/heads/main/work.ink-bypass.js
 // ==/UserScript==
