4f0510da6638ee1d7719bcb904f2b420119562f4
Huyyhere <bquangghuyy2011@gmail.com>
2025-11-01 23:22:56 +0700

    Update rekonise.user.js

--- Rekonise/rekonise.user.js
+++ Rekonise/rekonise.user.js
@@ -1,1 +1,14 @@
+// ==UserScript==
+// @name         Bypass Rekonise
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Bypass Rekonise
+// @author       Huyyhere
+// @match        https://rekonise.com/*
+// @grant        unsafeWindow
+// @run-at       document-start
+// @downloadURL  https://raw.githubusercontent.com/Huyyhere/bypass-link/refs/heads/main/Rekonise/rekonise-bypass.js
+// @updateURL    https://raw.githubusercontent.com/Huyyhere/bypass-link/refs/heads/main/Rekonise/rekonise-bypass.js
+// @require      https://raw.githubusercontent.com/Huyyhere/bypass-link/refs/heads/main/Rekonise/rekonise-bypass.js
+// ==/UserScript==
 
